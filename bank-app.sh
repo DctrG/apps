92328ecdbb8bf1d61d64a454f8566b3b25ec8f0f
Guillermo Serrano <gserrano@paloaltonetworks.com>
2024-11-13 13:54:59 +0000

    add absolute path to venv

--- bank-app.sh
+++ bank-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source venv/bin/activate
+source /root/venv/bin/activate
 cd /home/paloalto/apps/
 nohup streamlit run bank-app.py   --browser.serverAddress=localhost   --server.enableCORS=false —server.enableXsrfProtection=false   --server.port 80
