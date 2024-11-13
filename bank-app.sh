2c3a641e3f096ad2802e2e8461a627ebd81d69af
Guillermo Serrano <gserrano@paloaltonetworks.com>
2024-11-13 06:15:45 +0000

    moved startup script to Terraform

--- bank-app.sh
+++ bank-app.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export SSL_CERT_FILE=/etc/ssl/certs/root_ca.pem
-export REQUESTS_CA_BUNDLE=/etc/ssl/certs/root_ca.pem
-export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH=/etc/ssl/certs/root_ca.pem
 
 source venv/bin/activate
 cd /home/paloalto/apps/
